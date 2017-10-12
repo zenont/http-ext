54b8b6a2a675f2d27112023d4afbbf6a18843694
Zenon Trujillo <trujillo.zenon@outlook.com>
2017-10-11 22:34:23 -0700

    * HttpClientExt.UnitTests.csproj: fixing unit tests
    * QueryStringTest.cs:

--- test/HttpClientExt.UnitTests/QueryStringTest.cs
+++ test/HttpClientExt.UnitTests/QueryStringTest.cs
@@ -1,330 +1,334 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
 using HttpClientExt.Common;
 using Xunit;
 
 namespace HttpClientExt.UnitTests
 {
-public class QueryStringTest
-	{
-		[Theory]
-		[InlineData("someKey", true, "True")]
-		[InlineData("someKey", 10, "10")]
-		[InlineData("someKey", 10.103, "10.103")]
-		[InlineData("someKey", "10.103", "10.103")]
-		public void ShouldAddKeyValueStrings(string key, object value, string expected)
-		{
-			// arrange
-			QueryString queryString = new QueryString();
+    public class QueryStringTest
+    {
+        [Theory]
+        [InlineData("someKey", true, "True")]
+        [InlineData("someKey", 10, "10")]
+        [InlineData("someKey", 10.103, "10.103")]
+        [InlineData("someKey", "10.103", "10.103")]
+        public void ShouldAddKeyValueStrings(string key, object value, string expected)
+        {
+            // arrange
+            QueryString queryString = new QueryString();
 
-			// act
-			queryString.Add(key, value?.ToString());
+            // act
+            queryString.Add(key, value?.ToString());
 
-			// assert
-			Assert.True(queryString.Single().Value == expected);
-		}
+            // assert
+            Assert.True(queryString.Single().Value == expected);
+        }
 
-		[Theory]
-		[InlineData("someKey", new int[] { 1, 2, 3, 4 }, new string[] { "1", "2", "3", "4" })]
-		public void SHouldAddArray(string key, int[] values, string[] expected)
-		{
-			// arrange
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v.ToString());
-			}
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+        [Theory]
+        [InlineData("someKey", new int[] { 1, 2, 3, 4 }, new string[] { "1", "2", "3", "4" })]
+        public void SHouldAddArray(string key, int[] values, string[] expected)
+        {
+            // arrange
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v.ToString());
+            }
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddListAndIgnoreNullItem()
-		{
-			// arrange
-			const string key = "someKey";
-			List<int?> values = new List<int?> { 1, 2, null, 4 };
-			var expected = new string[] { "1", "2", "4" };
+        [Fact]
+        public void ShouldAddListAndIgnoreNullItem()
+        {
+            // arrange
+            const string key = "someKey";
+            List<int?> values = new List<int?> { 1, 2, null, 4 };
+            var expected = new string[] { "1", "2", "4" };
 
-			// act
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v?.ToString());
-			}
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+            // act
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v?.ToString());
+            }
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddListWithDatetimes()
-		{
-			// arrange
-			const string key = "someKey";
-			var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
-			var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
-			var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
+        [Fact]
+        public void ShouldAddListWithDatetimes()
+        {
+            // arrange
+            const string key = "someKey";
+            var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
+            var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
+            var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
 
-			var values = new List<DateTime> { value1, value2, value3 };
-			var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
+            var values = new List<DateTime> { value1, value2, value3 };
+            var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
 
-			// act
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v);
-			}
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+            // act
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v);
+            }
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddListWithDatetimesCastedToParams()
-		{
-			// arrange
-			const string key = "someKey";
-			var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
-			var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
-			var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
+        [Fact]
+        public void ShouldAddListWithDatetimesCastedToParams()
+        {
+            // arrange
+            const string key = "someKey";
+            var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
+            var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
+            var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
 
-			var values = new List<DateTime> { value1, value2, value3 };
-			var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
+            var values = new List<DateTime> { value1, value2, value3 };
+            var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
 
-			// act
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v);
-			}
+            // act
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v);
+            }
 
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddArrayWithDatetimes()
-		{
-			// arrange
-			const string key = "someKey";
-			var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
-			var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
-			var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
+        [Fact]
+        public void ShouldAddArrayWithDatetimes()
+        {
+            // arrange
+            const string key = "someKey";
+            var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
+            var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
+            var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
 
-			var values = new DateTime[] { value1, value2, value3 };
-			var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
+            var values = new DateTime[] { value1, value2, value3 };
+            var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
 
-			// act
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v);
-			}
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+            // act
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v);
+            }
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddArrayWithDatetimesCastedAsParams()
-		{
-			// arrange
-			const string key = "someKey";
-			var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
-			var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
-			var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
+        [Fact]
+        public void ShouldAddArrayWithDatetimesCastedAsParams()
+        {
+            // arrange
+            const string key = "someKey";
+            var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
+            var value2 = new DateTime(2010, 2, 1, 10, 0, 0);
+            var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
 
-			var values = new DateTime[] { value1, value2, value3 };
-			var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
+            var values = new DateTime[] { value1, value2, value3 };
+            var expected = new string[] { value1.ToString("o"), value2.ToString("o"), value3.ToString("o") };
 
-			// act
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v);
-			}
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+            // act
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v);
+            }
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddListWithNullableDatetimes()
-		{
-			// arrange
-			const string key = "someKey";
-			var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
-			DateTime? value2 = null;
-			var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
+        [Fact]
+        public void ShouldAddListWithNullableDatetimes()
+        {
+            // arrange
+            const string key = "someKey";
+            var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
+            DateTime? value2 = null;
+            var value3 = new DateTime(2010, 3, 1, 10, 0, 0);
 
-			List<DateTime?> values = new List<DateTime?> { value1, value2, value3 };
-			var expected = new string[] { value1.ToString("o"), value3.ToString("o") };
+            List<DateTime?> values = new List<DateTime?> { value1, value2, value3 };
+            var expected = new string[] { value1.ToString("o"), value3.ToString("o") };
 
-			// act
-			QueryString queryString = new QueryString();
-			foreach (var v in values)
-			{
-				queryString.Add(key, v);
-			}
-			KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
+            // act
+            QueryString queryString = new QueryString();
+            foreach (var v in values)
+            {
+                queryString.Add(key, v);
+            }
+            KeyValuePair<string, string>[] parsedValue = queryString.Where(x => x.Key == key).ToArray();
 
-			// assert
-			for (int i = 0; i < expected.Length; i++)
-			{
-				Assert.True(parsedValue[i].Value == expected[i]);
-			}
-		}
+            // assert
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.True(parsedValue[i].Value == expected[i]);
+            }
+        }
 
-		[Fact]
-		public void ShouldAddDateTime()
-		{
-			// arrange
-			const string key = "key1";
-			var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
-			QueryString queryString = new QueryString();
+        [Fact]
+        public void ShouldAddDateTime()
+        {
+            // arrange
+            const string key = "key1";
+            var value1 = new DateTime(2010, 1, 1, 10, 0, 0);
+            QueryString queryString = new QueryString();
 
-			// act
-			queryString.Add(key, value1);
+            // act
+            queryString.Add(key, value1);
 
-			// assert
-			Assert.True(queryString.Single().Value == value1.ToString("o"));
-		}
+            // assert
+            Assert.True(queryString.Single().Value == value1.ToString("o"));
+        }
 
-		[Fact]
-		public void ShouldAddNullableDateTime()
-		{
-			// arrange
-			const string key = "key1";
-			DateTime? value1 = null;
-			QueryString queryString = new QueryString();
+        [Fact]
+        public void ShouldAddNullableDateTime()
+        {
+            // arrange
+            const string key = "key1";
+            DateTime? value1 = null;
+            QueryString queryString = new QueryString();
 
-			// act
-			queryString.Add(key, value1?.ToString());
+            // act
+            queryString.Add(key, value1?.ToString());
 
-			// assert
-			Assert.Empty(queryString);
-		}
+            // assert
+            Assert.Empty(queryString);
+        }
 
-		[Fact]
-		public async Task ShouldConvertValuesToUrl()
-		{
-			// arrange
-			QueryString queryString = new QueryString();
+        [Fact]
+        public async Task ShouldConvertValuesToUrl()
+        {
+            // arrange
+            QueryString queryString = new QueryString();
 
-			// act
-			queryString.Add("key1", "value1");
-			queryString.Add("key2", "value2");
-			string url = await queryString.AsUrlAsync();
+            // act
+            queryString.Add("key1", "value1");
+            queryString.Add("key2", "value2");
+            string url = await queryString.AsUrlAsync();
 
-			// assert
-			Assert.True(url == "key1=value1&key2=value2");
-		}
+            // assert
+            Assert.True(url == "key1=value1&key2=value2");
+        }
 
-		[Fact]
-		public async Task ShouldConvertArrayToUrl()
-		{
-			// arrange
-			QueryString queryString = new QueryString();
-			var values = new int[] { 1, 2, 3 };
+        [Fact]
+        public async Task ShouldConvertArrayToUrl()
+        {
+            // arrange
+            QueryString queryString = new QueryString();
+            var values = new int[] { 1, 2, 3 };
 
-			// act
-			foreach (var v in values)
-			{
-				queryString.Add("key1", v.ToString());
-			}
-			queryString.Add("key2", "value2");
-			string url = await queryString.AsUrlAsync();
+            // act
+            foreach (var v in values)
+            {
+                queryString.Add("key1", v.ToString());
+            }
+            queryString.Add("key2", "value2");
+            string url = await queryString.AsUrlAsync();
 
-			// assert
-			Assert.True(url == "key1=1&key1=2&key1=3&key2=value2");
-		}
+            // assert
+            Assert.True(url == "key1=1&key1=2&key1=3&key2=value2");
+        }
 
-		[Fact]
-		public async Task ShouldConvertEmptyCollectionEdgeCaseToUrl()
-		{
-			// arrange
-			QueryString queryString = new QueryString();
+        [Fact]
+        public async Task ShouldConvertEmptyCollectionEdgeCaseToUrl()
+        {
+            // arrange
+            QueryString queryString = new QueryString();
 
-			// act
-			string url = await queryString.AsUrlAsync();
+            // act
+            string url = await queryString.AsUrlAsync();
 
-			// assert
-			Assert.True(url == "");
-		}
+            // assert
+            Assert.True(url == "");
+        }
 
-		[Fact]
-		public async Task ShouldConvertNullValueEdgeCaseToUrl()
-		{
-			// arrange
-			QueryString queryString = new QueryString();
+        [Fact]
+        public async Task ShouldConvertNullValueEdgeCaseToUrl()
+        {
+            // arrange
+            QueryString queryString = new QueryString();
 
-			// act
-			queryString.Add("key1", "");
-			string url = await queryString.AsUrlAsync();
+            // act
+            queryString.Add("key1", "");
+            string url = await queryString.AsUrlAsync();
 
-			// assert
-			Assert.True(url == "");
-		}
+            // assert
+            Assert.True(url == "");
+        }
 
-		[Fact]
-		public async Task ShouldConvertArrayToUri()
-		{
-			// arrange
-			const string baseUrl = "http://localhost";
-			QueryString queryString = new QueryString();
-			var values = new int[] { 1, 2, 3 };
+        [Fact]
+        public async Task ShouldConvertArrayToUri()
+        {
+            // arrange
+            const string baseUrl = "http://localhost";
+            QueryString queryString = new QueryString();
+            var values = new int[] { 1, 2, 3 };
 
-			// act
-			foreach (var v in values)
-			{
-				queryString.Add("key1", v.ToString());
-			}
-			queryString.Add("key2", "value2");
-			Uri uri = await queryString.AsUriAsync(baseUrl);
+            // act
+            foreach (var v in values)
+            {
+                queryString.Add("key1", v.ToString());
+            }
+            queryString.Add("key2", "value2");
+            Uri uri = await queryString.AsUriAsync(baseUrl);
 
-			// assert
-			Assert.NotNull(uri);
-			Assert.True(uri.ToString() == "http://localhost/?key1=1&key1=2&key1=3&key2=value2");
-		}
+            // assert
+            Assert.NotNull(uri);
+            Assert.True(uri.ToString() == "http://localhost/?key1=1&key1=2&key1=3&key2=value2");
+        }
 
-		[Fact]
-		public void ShouldThrowWhenKeyIsNull()
-		{
-			// arrange
-			QueryString queryString = new QueryString();
+        [Fact]
+        public void ShouldThrowWhenKeyIsNull()
+        {
+            // arrange
+            QueryString queryString = new QueryString();
 
-			// act
-			Action action = () => queryString.Add("", "value2");
+            // act
+            Action action = () => queryString.Add("", "value2");
 
-			// assert
-			action.ShouldThrow<ArgumentNullException>();
-		}
-	}
+            // assert
+            action.ShouldThrow<ArgumentNullException>();
+        }
+    }
 }
